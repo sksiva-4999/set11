b8c5f94c3cbca8cbfdbcb919ad94acffd78f18f9
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-14 11:42:45 +0530

    108.c

---
+++ 108.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,k;
+scanf("%d%d",&n,&k);
+
+if(k<=n)
+   {
+    printf("%d",k);
+   }
+    else
+    {
+    printf("The smallest values:%d",n);	
+    }
+ return 0; 
+}
